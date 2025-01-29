b267b5634165aa81ad913c3e9215f55e0302e201
bl896211_gsk <bernardo.x.lares@gsk.com>
2025-01-29 12:37:07 +0100

    mdynstdgrfs

--- demo/demo.R
+++ demo/demo.R
@@ -1,7 +1,10 @@
 library(reticulate)
+
 virtualenv_create("r-reticulate")
 use_virtualenv("r-reticulate", required = TRUE)
-py_install("google-meridian", ignore_installed = FALSE, pip = TRUE)
+
+# Install from local directory to have more control on versions
+py_install("meridian", ignore_installed = FALSE, pip = TRUE)
 
 # Import libraries
 np <- import("numpy", delay_load = FALSE)
@@ -11,7 +14,7 @@ tfp <- import("tensorflow_probability", delay_load = FALSE)
 az <- import("arviz", delay_load = FALSE)
 meridian <- import("meridian", delay_load = FALSE)
 
-# Import the necessary Python modules
+# Import the necessary modules
 constants <- import("meridian.constants")
 load <- import("meridian.data.load")
 test_utils <- import("meridian.data.test_utils")
@@ -25,7 +28,14 @@ summarizer <- import("meridian.analysis.summarizer")
 formatter <- import("meridian.analysis.formatter")
 
 # Check if GPU is available
-tensorflow::tf_gpu_configured()
+try(tensorflow::tf_gpu_configured())
+
+######## STEP 1: LOAD THE DATA
+
+# Load (dummy) data to inspect
+csv_path <- "meridian/data/simulated_data/csv/geo_all_channels.csv"
+df <- read.csv(csv_path)
+head(df, 10)
 
 # Map the column names to their corresponding variable types
 # Defs: https://developers.google.com/meridian/docs/user-guide/collect-data
@@ -34,7 +44,7 @@ coord_to_columns <- load$CoordToColumns(
   geo = 'geo',
   controls = c('GQV', 'Competitor_Sales'),
   population = 'population',
-  kpi = 'conversions',
+  kpi = 'revenue_per_conversion',
   revenue_per_kpi = 'revenue_per_conversion',
   media = c(
     'Channel0_impression',
@@ -71,14 +81,9 @@ correct_media_spend_to_channel <- dict(
   'Channel4_spend' = 'Channel_4'
 )
 
-# Load the (dummy) data
-csv_file <- "meridian/data/simulated_data/csv/geo_all_channels.csv"
-df <- read.csv(csv_file)
-head(df, 10)
-
 # Load the data
 loader <- load$CsvDataLoader(
-  csv_path = csv_file,
+  csv_path = csv_path,
   kpi_type = 'non_revenue',
   coord_to_columns = coord_to_columns,
   media_to_channel = correct_media_to_channel,
@@ -87,3 +92,5 @@ loader <- load$CsvDataLoader(
 # Error in py_call_impl(callable, call_args$unnamed, call_args$named) : 
 #   AttributeError: 'Series' object has no attribute 'stack'
 data <- loader$load()
+
+######## STEP 2: CONFIGURE THE MODEL
